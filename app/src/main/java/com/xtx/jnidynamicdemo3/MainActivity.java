a74a89a67826d89cbf207037651e704039ace382
xtx321 <xietianxin2013@163.com>
2019-11-16 22:52:47 +0800

    add log

--- app/src/main/java/com/xtx/jnidynamicdemo3/MainActivity.java
+++ app/src/main/java/com/xtx/jnidynamicdemo3/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn1).setOnClickListener(this);
+        Log.d(TAG,"onCreate");
     }
 
     @Override
